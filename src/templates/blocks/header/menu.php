33288fb7df72a06850b7970b9776782e40e84acc
Nicolas Fiascaro <n.fiascaro@gmail.com>
2018-11-14 14:41:19 +0100

    More menu more test :)

--- src/templates/blocks/header/menu.php
+++ src/templates/blocks/header/menu.php
@@ -22,13 +22,68 @@
                 </li>
                 <li class="menu-item">
                     <a href="#">Sub menu item 2</a>
+                    <ul class="sub-menu">
+                        <li class="menu-item">
+                            <a href="#">More submenu item 1</a>
+                        </li>
+                        <li class="menu-item">
+                            <a href="#">More submenu item 1</a>
+                        </li>
+                        <li class="menu-item">
+                            <a href="#">More submenu item 1</a>
+                        </li>
+                        <li class="menu-item">
+                            <a href="#">More submenu item 1</a>
+                        </li>
+                    </ul>
                 </li>
                 <li class="menu-item">
                     <a href="#">Sub menu item 3</a>
                 </li>
             </ul>
         </li>
-        <li class="menu-item"><a href="02-page-default.php">Page default</a></li>
+        <li class="menu-item">
+            <a href="02-page-default.php">Page default</a>
+            <ul class="sub-menu">
+                <li class="menu-item menu-item-has-children">
+                    <a href="#">Sub menu item 1</a>
+                    <ul class="sub-menu">
+                        <li class="menu-item">
+                            <a href="#">More submenu item 1</a>
+                        </li>
+                        <li class="menu-item">
+                            <a href="#">More submenu item 1</a>
+                        </li>
+                        <li class="menu-item">
+                            <a href="#">More submenu item 1</a>
+                        </li>
+                        <li class="menu-item">
+                            <a href="#">More submenu item 1</a>
+                        </li>
+                    </ul>
+                </li>
+                <li class="menu-item">
+                    <a href="#">Sub menu item 2</a>
+                    <ul class="sub-menu">
+                        <li class="menu-item">
+                            <a href="#">More submenu item 1</a>
+                        </li>
+                        <li class="menu-item">
+                            <a href="#">More submenu item 1</a>
+                        </li>
+                        <li class="menu-item">
+                            <a href="#">More submenu item 1</a>
+                        </li>
+                        <li class="menu-item">
+                            <a href="#">More submenu item 1</a>
+                        </li>
+                    </ul>
+                </li>
+                <li class="menu-item">
+                    <a href="#">Sub menu item 3</a>
+                </li>
+            </ul>
+        </li>
         <li class="menu-item"><a href="03-archive-default.php">Archive default</a></li>
         <li class="menu-item"><a href="04-page-404.php">Page 404</a></li>
     </ul>
