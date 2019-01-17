0602829bc317766bd71db4a7174d0db5bb707b06
Benjamin Bertout <bertout.benjamin@gmail.com>
2019-01-17 13:57:35 +0100

    add click outside ready example

--- src/js/src/scripts-domready.js
+++ src/js/src/scripts-domready.js
@@ -2,6 +2,13 @@
  * Init stuffs
  */
 
-// document.addEventListener('DOMContentLoaded', () => {
-//   // code
-// })
+document.addEventListener('DOMContentLoaded', () => {
+  // Click outside
+  /* document.addEventListener('click', event => {
+    const el = document.querySelector('#el').contains(event.target)
+    const elContainer = document.querySelector('#elContainer').contains(event.target)
+    if (!el && !elContainer) {
+      document.body.classList.remove('example')
+    }
+  }) */
+})
