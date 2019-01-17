a32bf14b18074d77a912c7f2956214e5f0d4b831
Benjamin Bertout <bertout.benjamin@gmail.com>
2019-01-17 13:45:55 +0100

    fix package and server for windows

--- config/server.js
+++ config/server.js
@@ -5,7 +5,8 @@ const config = require('./config')
 webpackDev.plugins.push(
   new BrowserSyncPlugin(
     {
-      proxy: 'http://[::1]:' + config.port,
+      // proxy: 'http://[::1]:' + config.port, // Mac OS
+      proxy: 'http://localhost:' + config.port, // Win 10
       files: [
         {
           match: config.refresh,
